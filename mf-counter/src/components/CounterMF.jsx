4d8d710239d6e4b4ae18440b74c5bd07e2e5601c
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-04-27 16:50:48 -0500

    counter

---
+++ mf-counter/src/components/CounterMF.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import './CounterMF.css';
+
+const CounterMF = ({ initialCounter }) => {
+    const [counter, setCounter] = useState(initialCounter);
+
+    useEffect(() => {
+        setCounter(initialCounter); // Actualiza el contador cuando la prop initialCounter cambia
+    }, [initialCounter]);
+
+    const handleIncrement = () => {
+        setCounter(counter + 1);
+    };
+
+    const handleDecrement = () => {
+        setCounter(counter - 1);
+    };
+
+    const getCounterColor = () => {
+        if (counter < 0) {
+            return 'red';
+        } else if (counter === 0 || counter > 0) {
+            return 'green';
+        }
+    };
+
+    return (
+        <div className="container"> 
+            <h3>Counter MF</h3>
+            <div className="button-container">
+                <button className="button increment" onClick={handleIncrement}>+</button>
+                <h4 className="counter" style={{ color: getCounterColor() }}>{counter}</h4>
+                <button className="button decrement" onClick={handleDecrement}>-</button>
+            </div>
+        </div>
+    );
+};
+
+export default CounterMF;
